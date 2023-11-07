987d959930d6af0bc31e82aa9827dc468a328443
igorcamargo1 <rm94553@fiap.com.br>
2023-11-07 14:42:47 -0300

    projeto adicionado

---
+++ SnowPark/SnowPark/Models/Compra.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SnowPark.Models;
+
+[Table("Tbl_Compra")]
+public class Compra
+{
+    [Column("CompraId"), HiddenInput]
+    public int CompraId { get; set; }
+
+    [Required, MaxLength(80)]
+    public string? Nome { get; set; }
+
+    [Required, MaxLength(11)]
+    public string? Cpf { get; set; }
+
+    [Column("Dt_Visita"), Display(Name = "Data de Visita"),
+         DataType(DataType.Date)]
+    public DateTime DataVisita { get; set; }
+
+    [Column("Ds_Servico"), Required, Display(Name = "Serviço")]
+    public Servico Servico { get; set; }
+
+}
+
+public enum Servico
+{
+    Teleferico, SnowMobile, PacoteCompleto, Esquiar
+}
