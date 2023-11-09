b2a19ba7ad76e8442ce4eb2671bd7c51a172cc93
igorcamargo1 <rm94553@fiap.com.br>
2023-11-09 11:13:01 -0300

    mvc basico feito

---
+++ SnowPark/SnowPark/Controllers/CompraController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SnowPark.Controllers;
+
+public class CompraController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ SnowPark/SnowPark/Persistencia/ParqueContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SnowPark.Models;
+
+namespace SnowPark.Persistencia;
+
+//Classe que gerencia as entidades
+public class ParqueContext : DbContext
+{
+    public DbSet<Compra> Compras { get; set; }
+
+    public ParqueContext(DbContextOptions op) : base(op) { }
+
+
+}
